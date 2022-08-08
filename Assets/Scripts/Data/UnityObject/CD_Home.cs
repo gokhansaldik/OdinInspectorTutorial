538dd735744d8cd09bd487792e93ab0aa20da2a5
MCGelerin <m.cangelerin@gmail.com>
2022-08-08 19:17:41 +0300

    Home Builder Added

---
+++ Assets/Scripts/Data/UnityObject/CD_Home.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using Data.ValueObject;
+
+namespace Data.UnityObject
+{
+    [CreateAssetMenu(fileName = "HomeData", menuName = "My Game/Home Data")]
+    public class CD_Home : ScriptableObject
+    {
+        [BoxGroup("Basic Info")]
+        [LabelWidth(100)]
+        public string BuildName;
+
+        [BoxGroup("Basic Info")]
+        [LabelWidth(100)]
+        [TextArea]
+
+        public string Description;
+
+
+        public List<HomeValues> HomeSetting = new List<HomeValues>();
+
+    }
+}
---
+++ Assets/Scripts/Editor/HomeBuilder.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using Data.UnityObject;
+
+namespace OdinEditor
+{
+    public class HomeBuilder : OdinMenuEditorWindow
+    {
+        [MenuItem("Tools/Home Data")]
+        private static void OpenWindow()
+        {
+            GetWindow<HomeBuilder>().Show();               // Tools Kısmını oluşturdu
+        }
+
+        private CreateNewBuildData _createNewBuildData;
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            if (_createNewBuildData != null)
+            {
+                DestroyImmediate(_createNewBuildData.HomeData);
+            }
+        }
+
+        
+
+        protected override OdinMenuTree BuildMenuTree()
+        {
+            var tree = new OdinMenuTree();
+            _createNewBuildData = new CreateNewBuildData();
+            tree.Add("Create New", _createNewBuildData);
+            tree.AddAllAssetsAtPath("HomeData", "Assets/Resources/Data", typeof(CD_Home));
+
+            return tree;
+        }
+
+        protected override void OnBeginDrawEditors()
+        {
+            OdinMenuTreeSelection selected = this.MenuTree.Selection;
+            SirenixEditorGUI.BeginHorizontalToolbar();
+            {
+                GUILayout.FlexibleSpace();
+
+                if (SirenixEditorGUI.ToolbarButton("Delete Current"))
+                {
+                    CD_Home asset = selected.SelectedValue as CD_Home;
+                    string path = AssetDatabase.GetAssetPath(asset);
+                    AssetDatabase.DeleteAsset(path);
+                    AssetDatabase.SaveAssets();
+                }
+            }
+
+            SirenixEditorGUI.EndHorizontalToolbar();
+        }
+
+
+        public class CreateNewBuildData
+        {
+            public CreateNewBuildData()
+            {
+                HomeData = ScriptableObject.CreateInstance<CD_Home>();
+                HomeData.BuildName = "New Build Data";
+            }
+            [InlineEditor(ObjectFieldMode = InlineEditorObjectFieldModes.Hidden)]
+            public CD_Home HomeData;
+
+
+            //Create new instance of the CD
+            [Button("Add New Build CD")]
+            private void CreateNewData()
+            {
+                AssetDatabase.CreateAsset(HomeData, "Assets/Resources/Data/" + HomeData.BuildName + ".asset");
+                AssetDatabase.SaveAssets();
+
+                HomeData = ScriptableObject.CreateInstance<CD_Home>();
+                HomeData.BuildName = "New Build Data";
+            }
+        }
+
+
+    }
+}
